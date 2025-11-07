2894d2e03f11d1e9841cab2c221da7854fd1d1f6
Srizany <nasirahusnasrizany@gmail.com>
2025-11-07 21:34:24 +0600

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,19 @@
+x = "Fatema"
+y = "Ariz"
+z = "Srizany"
+
+a = int("4_5_2011")
+b = int("14_5_2011")
+c = int("30_10_2010")
+
+name = input("Enter your name: ")
+print(name)
+
+if (name==x):
+    print("Hello, your birthdate is: ", a)
+
+if (name==y):
+    print("Hello, your birthdate is: ", b)
+
+if (name==z):
+    print("Hello, your birthdate is: ", c)
